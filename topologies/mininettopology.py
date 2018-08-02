f5c3cfe59b671886cd250f67a41be7ad8411ffe5
megawoman001 <40152614+megawoman001@users.noreply.github.com>
2018-08-02 13:29:14 -0400

    Update mininettopology.py

--- topologies/mininettopology.py
+++ topologies/mininettopology.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 #Topology with three switches, victim host, attacker host, and the controller
 
-from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import RemoteController
 from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
-from mininet.log import setLogLevel
+from mininet.log import info, setLogLevel
 setLogLevel('info')
 
+self = Mininet(controller=RemoteController)
 info('*** Adding controller\n')
 self.addController( 'c0', controller=RemoteController, ip='172.31.2.32', port=6653 )
 
@@ -24,27 +24,22 @@ s3 = self.addSwitch ('s3')
 
 info ('***Adding links\n')
 self.addLink(s1, s2, port1=2, port2=2, cls=TCLink, delay='100ms', bw=1)
-self.addLink(s2, s1, port1=2, port2=2, cls=TCLink, delay='100ms', bw=1)
 self.addLink(s2, s3, port1=3, port2=3, cls=TCLink, delay='100ms', bw=1)
-self.addLink(s3, s2, port1=3, port2=3, cls=TCLink, delay='100ms', bw=1)
 self.addLink(s3, s1, port1=2, port2=3, cls=TCLink, delay='100ms', bw=1)
-self.addLink(s1, s3, port1=3, port2=2, cls=TCLink, delay='100ms', bw=1)
 self.addLink(h1, s1, port1=1, port2=1, cls=TCLink, delay='100ms', bw=1)
-self.addLink(s1, h1, port1=1, port2=1, cls=TCLink, delay='100ms', bw=1)
 self.addLink(h2, s2, port1=1, port2=1, cls=TCLink, delay='100ms', bw=1)
-self.addLink(s2, h2, port1=1, port2=1, cls=TCLink, delay='100ms', bw=1)
 
 info('*** Starting network\n')
-net.start()
+self.start()
 print "Dumping host connections"
-dumpNodeConnections (net.hosts)
+dumpNodeConnections (self.hosts)
 
 info('*** Testing connectivity\n')
 print "Testing network connectivity"
-net.pingALL()
+self.pingALL()
 
 info('*** Running CLI\n')
-CLI(net)
+CLI(self)
 
 info('*** Stopping network')
-net.stop()
+self.stop()

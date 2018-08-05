e2badc2e1ef23a247c011f4cd704d556e0dcfe49
ackSec <jonmedina@gmail.com>
2018-08-05 01:07:19 -0700

    commented out temporarily

--- labs/infra/workstation_route53.tf
+++ labs/infra/workstation_route53.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_route53_record" "workstation" {
   zone_id = "${var.zoneID["private"]}"
   name = "workstation"
@@ -5,3 +6,4 @@ resource "aws_route53_record" "workstation" {
   ttl = "300"
   records = ["${aws_instance.workstation.private_ip}"]
 }
+*/

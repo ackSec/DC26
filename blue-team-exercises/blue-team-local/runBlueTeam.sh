b95709293a9be847e86b5f3598fed97e20f83fcc
ackSec <jonmedina@gmail.com>
2018-08-06 17:56:27 -0700

    structure changes and code updates

--- blue-team-exercises/blue-team-local/runBlueTeam.sh
+++ blue-team-exercises/blue-team-local/runBlueTeam.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-sudo snort -A unsock -l /tmp -c /opt/etc/snort.conf -i eth0 &
-sleep 10
 sed -i '/controllerIP/'"$CONTROLLER_IP"'/g'sed -i 's/controllerIP/'"$CONTROLLER_IP"'/g' buildBlueTeam.py
 sudo -E python buildBlueTeam.py

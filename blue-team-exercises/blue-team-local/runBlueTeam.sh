dad87bb06054e792dcf4f04b95c124dbcdca41a2
ackSec <jonmedina@gmail.com>
2018-08-06 18:01:43 -0700

    I'm dumb

--- blue-team-exercises/blue-team-local/runBlueTeam.sh
+++ blue-team-exercises/blue-team-local/runBlueTeam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i '/controllerIP/'"$CONTROLLER_IP"'/g'sed -i 's/controllerIP/'"$CONTROLLER_IP"'/g' buildBlueTeam.py
+sed -i '/controllerIP/'"$CONTROLLER_IP"'/g' buildBlueTeam.py
 sudo -E python buildBlueTeam.py

029d60ef99afda5ceac14c92b03c707117c6b7ab
ackSec <jonmedina@gmail.com>
2018-08-06 17:40:29 -0700

    folder updates and minor tweaks

--- blue-team/runBlueTeam.sh
+++ blue-team/runBlueTeam.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo snort -A unsock -l /tmp -c /opt/etc/snort.conf -i eth0 &
 sed -i 's/controllerIP/'"$CONTROLLER_IP"'/g'sed -i 's/controllerIP/'"$CONTROLLER_IP"'/g' buildBlueTeam.py
 sudo -E python buildBlueTeam.py

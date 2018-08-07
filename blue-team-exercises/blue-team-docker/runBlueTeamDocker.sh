276a711ab82a2a749142de51cd09b424095510b4
ackSec <jonmedina@gmail.com>
2018-08-06 18:06:08 -0700

    code fizes

--- blue-team-exercises/blue-team-docker/runBlueTeamDocker.sh
+++ blue-team-exercises/blue-team-docker/runBlueTeamDocker.sh
@@ -3,4 +3,4 @@
 sudo snort -A unsock -l /tmp -c /opt/etc/snort.conf -i eth0 &
 sleep 8
 sed -i 's/controllerIP/'"$CONTROLLER_IP"'/g' securityAppliance.py
-sudo -E securityAppliance.py
+python securityAppliance.py

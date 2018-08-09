e8d1a329e4cd8219ad93c4baaf95556aa877b6e5
ackSec <jonmedina@gmail.com>
2018-08-09 03:23:22 -0700

    major changes

--- sample-topologies/datacenterBasic.py
+++ sample-topologies/datacenterBasic.py
@@ -11,36 +11,41 @@ setLogLevel('info')
 
 
 
-class DatacenterBasicTopo (Topo):
-    "Datacenter topology with 4 hosts per rack, 4 racks, and a root switch"
-    controllerIP = repr(os.environ.get('CONTROLLER_IP'))
-    self = Containernet(controller=RemoteController)
-    info('*** Adding controller at '+ controllerIP + '\n')
-    self.addController('c0', controller=RemoteController, ip=controllerIP, port=6653)
 
-    def build(self):
-        self.racks = []
-        rootSwitch = self.addSwitch('s1')
-        for i in irange(1, 2):
-            rack = self.buildRack(i)
-            self.racks.append(rack)
-            for switch in rack:
-                self.addLink(rootSwitch, switch)
+"Datacenter topology with 4 hosts per rack, 4 racks, and a root switch"
+controllerIP = repr(os.environ.get('CONTROLLER_IP'))
+self = Containernet(controller=RemoteController)
+info('*** Adding controller at '+ controllerIP + '\n')
+self.addController('c0', controller=RemoteController, ip=controllerIP, port=6653)
 
-    def buildRack(self, loc):
-        "Build a rack of hosts with a top-of-rack switch"
 
-        dpid = (loc * 16) + 1
-        switch = self.addSwitch('s1r%s' % loc, dpid='%x' % dpid)
+self.racks = []
+rootSwitch = self.addSwitch('s1')
+for i in irange(1, 2):
+    rack = self.buildRack(i)
+    self.racks.append(rack)
+    for switch in rack:
+        self.addLink(rootSwitch, switch)
 
-        for n in irange(1, 5):
-            #host = self.addHost( 'h%sr%s' % ( n, loc ) )
-            #host = self.addHost('h%sr%s' % (n, loc), dimage="ubuntu:trusty")
-            host = self.addDocker('h%sr%s', dimage="acksec/dc26", environment={"CONTROLLER_IP": controllerIP}, working_dir="/root")
-            self.addLink(switch, host)
 
-        # Return list of top-of-rack switches for this rack
-        return [switch]
+"Build a rack of hosts with a top-of-rack switch"
+
+dpid = (loc * 16) + 1
+switch = self.addSwitch('s1r%s' % loc, dpid='%x' % dpid)
+
+for n in irange(1, 5):
+    #host = self.addHost( 'h%sr%s' % ( n, loc ) )
+    #host = self.addHost('h%sr%s' % (n, loc), dimage="ubuntu:trusty")
+    host = self.addDocker('h%sr%s', dimage="acksec/dc26", environment={"CONTROLLER_IP": controllerIP}, working_dir="/root")
+    self.addLink(switch, host)
+
+# Return list of top-of-rack switches for this rack
+info('*** Starting network\n')
+net.start()
+info('*** Running CLI\n')
+CLI(net)
+info('*** Stopping network')
+net.stop()
+
 
 # Allows the file to be imported using `mn --custom <filename> --topo dcbasic`
-topos = {'dcbasic': DatacenterBasicTopo}
